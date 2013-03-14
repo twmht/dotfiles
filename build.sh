53d3bfc29bb95bc5b8f9ff9b863ccb564282bfc3
mht <qrnnis2623891@gmail.com>
2013-03-14 20:01:33 +0800

    update

--- build.sh
+++ build.sh
@@ -3,10 +3,6 @@ mkdir -p ~/.vim
 mkdir -p ~/.vim/bundle && git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 ln -s ~/vimrc/.vimrc ~/.vimrc
 mkdir -p ~/.vim_plugin
-ln -s ~/vimrc/.vim_plugin/FuzzyFinder.vim ~/.vim_plugin/FuzzyFinder.vim
-ln -s ~/vimrc/.vim_plugin/neocomplcache.vim ~/.vim_plugin/neocomplcache.vim
-ln -s ~/vimrc/.vim_plugin/dbext.vim ~/.vim_plugin/dbext.vim
-ln -s ~/vimrc/.vim_plugin/tabline.vim ~/.vim_plugin/tabline.vim
 cp -r ~/vimrc/colors ~/.vim
 mkdir -p ~/.vim/ftplugin
 ln -s ~/vimrc/ftplugin/c_.vim ~/.vim/ftplugin/c_.vim
