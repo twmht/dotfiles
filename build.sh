069c814338c4a991d8e32b18e8fa23dd3e588691
Ming-Hsuan-Tu <qrnnis2623891@gmail.com>
2023-03-11 18:23:30 +0800

    add vimrc

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# for vim
+mkdir -p ~/.vim
+mkdir -p ~/.vim/bundle && git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+ln -s `pwd`/.vimrc ~/.vimrc
+# for nvim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir -p ~/.config/nvim
 ln -s `pwd`/init.nvim ~/.config/nvim/init.nvim
