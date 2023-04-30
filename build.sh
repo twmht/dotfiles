f3b6a5cee06a4b73931c8ef2c0b7478b0b2710ee
alec.tu <alec.tu@acer.com>
2023-04-30 04:29:34 +0000

    use init.lua

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
-#!/bin/bash
-# for vim
-# mkdir -p ~/.vim
-# mkdir -p ~/.vim/bundle && git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-# ln -s `pwd`/.vimrc ~/.vimrc
-# for nvim
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir -p ~/.config/nvim
-ln -s `pwd`/init.vim ~/.config/nvim/init.vim
+ln -s `pwd`/init.lua ~/.config/nvim/init.lua
+ln -s `pwd`/lua ~/.config/nvim/lua
 ln -s `pwd`/.tmux.conf ~/.tmux.conf
--- lua/config/tagbar.lua
+++ lua/config/tagbar.lua
@@ -1,3 +1,3 @@
 vim.g.tagbar_autofocus = 1
 vim.g.tagbar_autoclose = 1
-vim.keymap.set('n', 'tt', ':lua require("buffer_manager.ui").toggle_quick_menu()<CR>', {noremap=true, silent = true})
+vim.keymap.set('n', '<F9>', ':TagbarToggle<CR>', {noremap=true, silent = true, nowait = true})
